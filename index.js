2d21af1a31001b17c1f4641432586df18287d89a
andrey484 <vandrey866@gmail.com>
2017-09-15 14:42:45 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+git
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send('ti pidor')
 });
 
 app.listen(app.get('port'), function() {
