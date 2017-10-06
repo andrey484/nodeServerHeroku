66f200ed8e5b85e5e38f14ca06e701155cadb425
andrey <sdf>
2017-10-06 11:54:34 +0300

    fix error

--- route/getUserById.js
+++ route/getUserById.js
@@ -7,11 +7,14 @@ router.post('/', function (req, res) {
         (typeof req.query.userId === 'undefined' || req.query.userId === null)
     ){
         var error = {"error":"userId undefined or null"}
-        res.json(error)
+        res.json(error);
         return;
     }
     Model.UserModel.find({userId: req.query.userId}, function (err, docs) {
-        if(err) console.log(err)
+        if(docs.length == 0){
+            var error = {"error":"user with id not found"}
+            res.json(error)
+        }
         else{
             var json = {
                 id: docs[0].id,
