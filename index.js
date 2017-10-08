0b28ef27a6fe5b790b2a0c858ed79e1982ef137b
andrey <sdf>
2017-10-08 16:46:42 +0300

    fix

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var Model = require('./model/model');
-var Schema = mongoose.Schema;
-mongoose.connect('mongodb://andrey484:qwerty1234567@ds137464.mlab.com:37464/sunny_project');
-//mongoose.connect('mongodb://localhost/test');
-var server = require('http').createServer(app);
-var ws = require('ws').Server;
-var wss = new ws({app});
-var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Model = require('./model/model');
+const Schema = mongoose.Schema;
+//mongoose.connect('mongodb://andrey484:qwerty1234567@ds137464.mlab.com:37464/sunny_project');
+mongoose.connect('mongodb://localhost/test');
+const server = require('http').createServer(app);
+const SocketServer = require('ws').Server;
+const wss = new SocketServer({port:8080});
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-var getAllGames = require('./route/getAllGames');
-var getTeam = require('./route/getTeam');
-var getUserById = require('./route/getUserById');
+const getAllGames = require('./route/getAllGames');
+const getTeam = require('./route/getTeam');
+const getUserById = require('./route/getUserById');
 
 
 app.set('port', (process.env.PORT || 5000));
@@ -25,7 +25,7 @@ app.use('/getAllGames', getAllGames);
 app.use('/getTeam', getTeam);
 app.use('/getUserById', getUserById);
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
@@ -45,7 +45,7 @@ wss.on('connection', function (ws) {
     ws.on('message', function (data) {
         switch (JSON.parse(data).cmd) {
             case 10: {
-                var currentProgress = 0;
+                let currentProgress = 0;
                 Model.TeamModel.find({id: JSON.parse(data).teamId}, function (err, docs) {
                     if(err) console.log(err);
                     currentProgress = docs[0].progress;
