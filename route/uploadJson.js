e621facf34c2623915bd7646412e1ee0c7426cb4
andrey484 <vandrey866@gmail.com>
2017-10-30 15:14:22 +0200

    Update uploadJson.js

--- route/uploadJson.js
+++ route/uploadJson.js
@@ -11,7 +11,7 @@ router.post('/', function (req, res) {
             photoUrl: req.body.PhotoUrl,
             countOfTeams: req.body.CountOfTeams,
             isStart: req.body.IsStart,
-            isOver: req.Body.IsOver,
+            //isOver: req.Body.IsOver,
             task: req.body.Task
         });
 
